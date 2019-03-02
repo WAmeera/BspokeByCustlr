ccf554d7d2cd09c10e1dd073d447dd4b5b3116c9
VivianTham <45869006+VivianTham@users.noreply.github.com>
2019-03-02 22:52:24 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,30 +1,84 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Button } from 'react-native';
 
-import React from 'react';
-import {
-    StyleSheet,
-    Text,
-    View
-} from 'react-native';
-/*import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';*/
-import AppNavigator from './navigation/AppNavigator';
+import t from 'tcomb-form-native';
 
-export default class App extends React.Component {
-    render() {
-        return ( <
-            View style = {
-                styles.container
-            } >
-            <
-            Text > Welcome to Bspoke by Custlr! < /Text></View > );
+const Form = t.form.Form;
+
+const User = t.struct({
+  email: t.String,
+  username: t.maybe(t.String),
+  password: t.String,
+  terms: t.Boolean
+});
+
+const formStyles = {
+  ...Form.stylesheet,
+  formGroup: {
+    normal: {
+      marginBottom: 10
+    },
+  },
+  controlLabel: {
+    normal: {
+      color: 'blue',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    },
+    // the style applied when a validation error occours
+    error: {
+      color: 'red',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
     }
+  }
+}
+
+const options = {
+  fields: {
+    email: {
+      error: 'Without an email address how are you going to reset your password when you forget it?'
+    },
+    password: {
+      error: 'Choose something you use on a dozen other sites or something you won\'t remember'
+    },
+    terms: {
+      label: 'Agree to Terms',
+    },
+  },
+  stylesheet: formStyles,
+};
+
+export default class App extends Component {
+  handleSubmit = () => {
+    const value = this._form.getValue();
+    console.log('value: ', value);
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Form 
+          ref={c => this._form = c}
+          type={User} 
+          options={options}
+        />
+        <Button
+          title="Sign Up!"
+          onPress={this.handleSubmit}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
 });

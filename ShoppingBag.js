0f2818e3ea7df7f56e96611070282b82fffa4129
zcwong <32235937+zcwong@users.noreply.github.com>
2019-04-08 22:14:48 +0800

    Fix tab navigator bug

---
+++ ShoppingBag.js
@@ -0,0 +1,173 @@
+import React, {Component} from 'react';
+import {ScrollView,Dimensions,TouchableOpacity,Styles,AppRegistry,StyleSheet,Text,View,Button,FlatList,Image,ActivityIndicator,Divider, StatusBar} from 'react-native';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import * as firebase from 'firebase';
+
+
+/***************************class ShoppingBag*************************************/
+export default class ShoppingBag extends React.Component {
+   static navigationOptions = {
+          title: 'Cart     ',
+          tabBarIcon:({tintColor}) =>(
+        <View style={{paddingRight:5,paddingTop:2}}>
+ 
+          <Image source={require('./image/cart.png')}
+            style={{width:23,height:23,tintColor:'white'}}>
+            </Image>
+        </View>
+        )
+  };
+    //default constructor for class ShoppingBag
+    constructor (){
+      super()
+      this.state = {
+        dataSource : [],
+        isLoading: true,
+		currentUser : null
+      }
+    }
+  
+  componentWillMount (){
+   global.itemCount = 0;  
+   global.totalPrice = 0; 
+  firebase.auth().onAuthStateChanged((user) => {
+   const { currentUser } = firebase.auth();
+    this.setState ({currentUser});
+    const currentUser2 = this.state.currentUser;
+  
+  firebase.database().ref(`ShoppingBag/${currentUser2.uid}/cart/`).once('value', snapshot =>{
+   var ShoppingBag = [];
+     snapshot.forEach((child) => {
+       ShoppingBag.push({
+          brand: child.val().brand,
+          name: child.val().name,
+          Price: child.val().Price,
+          itemID : child.val().itemID,
+          Size: child.val().size,
+          Quantity: child.val().Quantity,
+          Photo1: child.val().Photo1,
+          category: child.val().category,
+          collar: child.val().collar,
+          shoulder: child.val().shoulder,
+          chest: child.val().chest,
+          sleeve: child.val().sleeve,
+          uniqueKey: child.key,
+		  
+       });
+        itemCount += 1; 
+        totalPrice += child.val().Price; 
+    });
+  this.setState({
+  dataSource : ShoppingBag
+ });
+ });
+ })};
+
+  //function renderItem - will show the informations fetched from the database to the user-end (the shopping bag list)
+  //function renderSeperator - will show a line, in this class it is used to seperate each items on the list
+    renderItem = ({item}) => {
+      const size = this.props.navigation.getParam('size', '');
+      var itemprice = item.Price;  
+      var uniqueKey = item.uniqueKey;  
+      return(
+        
+        <View style ={{flex: 1, flexDirection: 'row', marginBottom: 3}}>
+         <Image  style = {{width: 140, height: 190, margin: 5}}
+            source = {{url : item.Photo1}} />
+            <View style ={{flex:1, justifyContent: 'center', marginLeft :5}}>
+              <Text style  = {{fontSize: 20, color: 'black', marginBottom: 2, marginTop : 0}}>
+                {item.brand}
+              </Text>
+              <Text style  = {{fontSize: 16, color: 'black'}}>
+                {item.name}
+              </Text>
+              <Text style  = {{fontSize: 14, color: 'red', marginBottom: 15}}>
+                RM {item.Price}
+              </Text>
+              <Text style  = {{fontSize: 14, color: 'black'}}>
+                Size : {item.Size} 
+              </Text>
+              <Text style  = {{fontSize: 14, color: 'black', marginBottom: 2}}>
+                Measurement : {item.collar}cm (Collar) - {item.shoulder}cm (Shoulder) - {item.chest}cm (Chest) - {item.sleeve}cm (Sleeve)
+              </Text>
+              <Text style  = {{fontSize: 14, color: 'green'}}>
+                Quantity : {item.Quantity} 
+              </Text>
+               <Button onPress={() => this.deleteData(uniqueKey, itemprice)} title="Remove Item" color="black"/> 
+            </View>
+        </View>
+      )
+    }
+
+    deleteData  = (key, price)  => { 
+	const currentUser2 = this.state.currentUser;
+      totalPrice -= price;  
+      itemCount -= 1;
+      firebase.database().ref(`ShoppingBag/${currentUser2.uid}/cart/`).child(key).remove();
+      this.onDelete();
+    }
+
+    onDelete = () => {
+      this.componentWillMount ();
+    }
+
+    renderSeperator = () => {
+      return(
+        <View
+          style={{height: 1, width: '100%', backgroundColour : 'black'}}>
+        </View>
+      )
+    }
+
+
+
+    render () {
+      return (
+        <View style = {{flex: 1, alignContent: 'center'}}>
+        <ScrollView>
+          <FlatList
+            data = {this.state.dataSource}
+            renderItem = {this.renderItem}
+            /*keyExtractor = {name,index} => index]*/
+            ItemSeperatorComponent = {this.renderSeperator}
+          />
+          </ScrollView>
+          <View style={{ backgroundColor: '#AAAAAA'}}>
+            <Text></Text>
+            <Text style  = {{fontSize: 14, color: '#FFFF'}}>  Sub-total ({itemCount} item(s)) : RM {totalPrice} </Text> 
+            <Text></Text>
+            <Text style  = {{fontSize: 14, color: '#FFFF'}}>  Est. Shipping : FREE </Text>
+            <Text></Text>
+            <Text></Text>
+            <View style={{ borderBottomColor: '#FFFF', borderBottomWidth: 1, borderWidth: 0.5}}/>
+            <Text></Text>
+            <Text style  = {{fontSize: 14, color: '#FFFF'}}>  Grand total : RM {totalPrice} </Text>
+            <Text></Text>
+            <Text></Text>
+            <View style={{ borderBottomColor: '#FFFF', borderBottomWidth: 1, borderWidth: 0.5}}/>
+              <View style={{ backgroundColor: 'black'}}>
+                <Text></Text>
+                <Text></Text>
+                <Button title="Checkout" color= "black" onPress={() => this.props.navigation.navigate('Payment1')}/>
+                <Text></Text>
+                <Text></Text>
+              </View>
+            </View>
+        </View>
+    );
+  }
+} 
+
+
+const styles = StyleSheet.create({
+
+   img:{
+    width: Dimensions.get('window').width*0.66,
+      height:hp('10%'),
+
+    },
+  
+
+
+});
---
+++ payment3.js
@@ -0,0 +1,187 @@
+import React from 'react';
+import {TouchableOpacity,Dimensions,StyleSheet, Button, Text, View, Image,Switch} from 'react-native';
+import { createAppContainer, createStackNavigator, StackActions, NavigationActions } from 'react-navigation'; // Version can be specified in package.json
+import t from 'tcomb-form-native'; // 0.6.9
+import { CreditCardInput, LiteCreditCardInput } from "react-native-credit-card-input";
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import * as firebase from 'firebase';
+
+
+
+
+
+
+
+
+export default class Payment3 extends React.Component {
+
+    static navigationOptions = {
+      title: 'Payment           ',
+  };
+
+  constructor (){
+      super()
+      this.state = {
+        dataSource : [],
+        isLoading: true,
+		currentUser : null
+      }
+    }
+  
+  componentWillMount (){
+  firebase.auth().onAuthStateChanged((user) => {
+   global.itemCount = 0;  
+   global.totalPrice = 0; 
+    const { currentUser } = firebase.auth();
+    this.setState ({currentUser});
+    const currentUser2 = this.state.currentUser;
+  firebase.database().ref(`ShoppingBag/${currentUser2.uid}/cart/`).once('value', snapshot =>{
+   
+     snapshot.forEach((child) => {
+	   firebase.database().ref(`Shipping/${currentUser2.uid}/`)
+      .push({ item : child.val() });
+
+	  firebase.database().ref(`ShoppingBag/${currentUser2.uid}/`).remove();
+        itemCount += 1; 
+        totalPrice += child.val().Price; 
+    });
+ });
+ })};
+
+
+  handleSubmit = () => {
+  //  const value = this._form.getValue(); // use that ref to get the form value
+    this.props.navigation.navigate('Dashboard');
+    //console.log('value: ', value);
+  }
+
+  render() {
+    
+
+    return (
+      <View style={styles.container}>
+    
+
+         <View style={styles.infoContainer}>
+
+            <View style={styles.title}>
+              
+              <View style={styles.payment}>
+
+                <Text style={styles.mainTitle}> PAYMENT COMPLETED </Text>
+                  <Image 
+                    source={require('./image/check.png')}
+                    style={styles.image}
+                    />
+                </View>
+
+
+                
+
+            </View>
+
+
+         
+            
+
+
+
+              <View style={styles.button}>
+
+                 <TouchableOpacity activeOpacity={0.8} onPress={this.handleSubmit} >
+
+              <View style={styles.buttonContainer}>
+            
+                    <Image source={require('./image/home.png')}  style={styles.img}/>
+
+              </View>
+                </TouchableOpacity>
+
+          </View>
+        </View>
+
+      </View>
+    );
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    width:wp('100%'),
+    height:hp('100%'),
+
+  },
+
+  top: {
+       backgroundColor: '#8E8E8E',
+       width:wp('100%'),
+       alignItems: 'center',
+
+  },
+    header: {
+      fontSize: 30,
+      color: '#ffffff',
+
+    },
+
+     infoContainer: {
+            
+
+        width:wp('80%'),
+        height:hp('100%'),
+
+
+  },
+
+  payment:{
+    margin:20,
+    alignItems:'center',
+    width:wp('100%'),
+  },
+
+  image: {
+          margin:20,
+          width:60,
+          height:60,
+
+         },
+
+  title: {
+    alignItems:'center',
+    height: Dimensions.get('window').height*0.37,
+
+
+  },
+
+  mainTitle:{
+     fontSize: 20,
+     fontWeight: 'bold',
+     paddingTop:35,
+  },
+
+  button:{
+    alignItems:'center',
+    
+    
+
+  },
+
+  buttonContainer:{
+    paddingLeft:5,
+    width:"58%",
+
+    
+  },
+   img:{
+    width: Dimensions.get('window').width*0.45,
+      height:hp('10%'),
+
+    },
+  
+
+
+});
